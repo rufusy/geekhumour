b8befa14d4e3fd929518990b03fffbba3ac98e75
rufusy <ruffiam@yahoo.com>
2019-10-02 12:12:25 +0300

    Typehint with Routes interface

--- classes/Ninja/EntryPoint.php
+++ classes/Ninja/EntryPoint.php
@@ -15,7 +15,7 @@
          *
          * @return void
          */
-        public function __construct($route, $method, $routes)
+        public function __construct(string $route, string $method, \Ninja\Routes $routes)
         {
             $this->route = $route;
             $this->routes = $routes;
