132c994e356e65338efac145b5dbe5719c7eb56a
rufusy <ruffiam@yahoo.com>
2019-10-07 13:29:34 +0300

    Run method checks for the logged in user depending on the route requirements

--- classes/Ninja/EntryPoint.php
+++ classes/Ninja/EntryPoint.php
@@ -69,23 +69,32 @@
         public function run()
         {
             $routes = $this->routes->getRoutes();
-            $controller = $routes[$this->route][$this->method]['controller'];
-            $action = $routes[$this->route][$this->method]['action'];
+            $authentication = $this->routes->getAuthentication();
 
-            $page = $controller->$action();
-            
-            $title = $page['title'];
-
-            if (isset($page['variables']))
+            if (isset($routes[$this->route]['login']) && !$authentication->isLoggedIn()) 
             {
-                $output = $this->loadTemplate($page['template'], $page['variables']);
+                header('location: /login/error');
             }
-            else 
+            else
             {
-                $output = $this->loadTemplate($page['template']);
-            }
+                $controller = $routes[$this->route][$this->method]['controller'];
+                $action = $routes[$this->route][$this->method]['action'];
 
-            include __DIR__ . '/../../templates/layout.html.php';
+                $page = $controller->$action();
+                
+                $title = $page['title'];
+
+                if (isset($page['variables']))
+                {
+                    $output = $this->loadTemplate($page['template'], $page['variables']);
+                }
+                else 
+                {
+                    $output = $this->loadTemplate($page['template']);
+                }
+
+                include __DIR__ . '/../../templates/layout.html.php';
+            }
         }
 
     }
