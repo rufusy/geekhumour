dfd2619bbd8f3f891afd9d51401f4b2d3a507937
rufusy <ruffiam@yahoo.com>
2019-10-13 19:29:57 +0300

    use different templates for adding a new and editing a joke

---
+++ templates/addjoke.html.php
@@ -0,0 +1,12 @@
+<div style='margin-top:50px;' class="row">
+        <form class="col s12" action="/joke/store" method="post">
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea name="joke[joketext]" class="materialize-textarea"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <input class="btn waves-effect waves-light" type="submit" value="submit" name="submit">
+            </div>
+        </form>
+</div>
